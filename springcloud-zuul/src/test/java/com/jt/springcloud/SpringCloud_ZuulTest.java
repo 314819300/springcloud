9ae6b82592212f4cf24fc0d7f2b0f01e217e0eba
wangning <wangning@qq.com>
2021-03-02 16:55:08 +0800

    配置中心的使用

---
+++ springcloud-zuul/src/test/java/com/jt/springcloud/SpringCloud_ZuulTest.java
@@ -0,0 +1,11 @@
+package com.jt.springcloud;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * @author wangning
+ * @create 2021-03-02 16:54
+ */
+@SpringBootTest
+public class SpringCloud_ZuulTest {
+}
