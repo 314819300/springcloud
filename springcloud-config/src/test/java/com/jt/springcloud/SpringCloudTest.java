8a604dbfee77a5334e2ff9a551cd3ca7e3636e80
wangning <wangning@qq.com>
2021-03-02 16:53:41 +0800

    配置中心的使用

---
+++ springcloud-config/src/test/java/com/jt/springcloud/SpringCloudTest.java
@@ -0,0 +1,11 @@
+package com.jt.springcloud;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * @author wangning
+ * @create 2021-03-02 16:52
+ */
+@SpringBootTest
+public class SpringCloudTest {
+}
